1f131843b4a6a89779cc44e77b44047ae9ad6527
Riccardo Mancinelli <riccardo.mancinelli@cherrybytes.it>
2024-12-02 17:17:48 +0100

    -- save point

---
+++ head.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- import standard style -->
+<link rel="stylesheet" href="resources/css/standard.min.css">
+
+<!-- import header style -->
+<link rel="stylesheet" href="resources/css/header.min.css">
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>TrainingLab Javascript - Dynamic Library</title>
+    <?php require_once 'head.php'; ?>
+    <link rel="stylesheet" href="resources/css/index.min.css">
+</head>
+<body>
+    <main>
+        <h1>Welcome in RecensIbrary</h1>
+        <p>Here you can find all the books you want to read and review</p>
+        <a href="home.php?selected=1">Look more</a>
+    </main>
+</body>
+</html>
