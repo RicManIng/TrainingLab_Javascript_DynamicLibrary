07be50aadff7e2c80d3e3d1b7fe764da2d7f3c35
Riccardo Mancinelli <riccardo.mancinelliing@gmail.com>
2024-12-08 11:52:49 +0100

    login and registration styling

--- login.php
+++ login.php
@@ -32,14 +32,16 @@
             <section id="login-form">
                 <form action="login.php?selected=3&state=login" method="post">
                     <h1>Login</h1>
-                    <label for="username">Username</label>
+                    <label for="username">Username : </label>
                     <input type="text" name="username" id="username" required placeholder="insert your username">
-                    <label for="password">Password</label>
+                    <label for="password">Password : </label>
                     <input type="password" name="password" id="password" required placeholder="insert your password">
-                    <?php if($login_error): ?>
+                    <?php if(!$login_error): ?>
                         <p class="error">Invalid username or password</p>
                     <?php endif; ?>
                     <button type="submit">Login</button>
+                    <p>Don't have an account?</p>
+                    <a href="login.php?selected=3&state=register">Register</a>
                 </form>
             </section>
         <?php elseif($_GET['state'] === 'account'): ?>
@@ -51,6 +53,29 @@
                 session_destroy(); 
                 header('Location: login.php?selected=3&state=login'); 
             ?>
+        <?php elseif($_GET['state'] === 'register'): ?>
+            <section id="register-form">
+                <form action="login.php?selected=3&state=register" method="post">
+                    <h1>Register</h1>
+                    <label for="Name">Name * :</label>
+                    <input type="text" name="name" id="name" required placeholder="insert your name">
+                    <label for="lastname">Lastname * :</label>
+                    <input type="text" name="lastname" id="lastname" required placeholder="insert your lastname">
+                    <label for="email">Email * : </label>
+                    <input type="email" name="email" id="email" required placeholder="insert your email">
+                    <label for="username">Username * : </label>
+                    <input type="text" name="username" id="username" required placeholder="insert your username">
+                    <label for="age">Birth date * : </label>
+                    <input type="date" name="age" id="age" required placeholder="insert your birth date">
+                    <label for="password">Password * : </label>
+                    <input type="password" name="password" id="password" required placeholder="insert your password">
+                    <label for="password">Confirm Password * : </label>
+                    <input type="password" name="password" id="password" required placeholder="insert your password">
+                    <label for="account-image">Insert your account image</label>
+                    <input type="file" name="account-image" id="account-image">
+                    <button type="submit" disabled>Register</button>
+                </form>
+            </section>
         <?php endif; ?>
     </main>
 </body>
