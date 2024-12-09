35639c81b4dd9ad74bdda4b8c44bbde8d087b65f
Riccardo Mancinelli <riccardo.mancinelliing@gmail.com>
2024-12-09 20:13:19 +0100

    registration page completed

--- js/login.js
+++ js/login.js
@@ -10,27 +10,241 @@ function manageRegistrationForm(userArray){
         let password = document.getElementById('password').value;
         let confirmPassword = document.getElementById('confirm-password').value;
         let age = document.getElementById('age').value;
+        let button = document.querySelector('button[type="submit"]');
 
         let allCheckTrue = false;
 
         if (name != '' && lastname != '' && email != '' && username != '' && password != '' && confirmPassword != '' && age != ''){
             if (password == confirmPassword){
-                allCheckTrue = true;
+                if (email.indexOf('@') != -1 && email.indexOf('.') != -1){
+                    allCheckTrue = true;
+                }
             }
         }
-    }
-    let name = document.getElementById('name');
-    name.addEventListener('blur', function(){
-        if (name.value == ''){
-            let error = document.createElement('p');
-            error.innerHTML = 'Name is required';
-            error.id = 'name-error';
-            name.insertAfter(error);
+
+        if (allCheckTrue){
+            button.disabled = false;
         } else {
-            let error = document.getElementById('name-error');
-            if (error){
-                error.remove();
-            }
+            button.disabled = true;
+        }
+    }
+    let name = $('#name');
+    name.on('blur', function(){
+        if (name.val() === ''){
+            // Rimuovi eventuali messaggi di errore precedenti
+            $('#name-error').remove();
+            
+            // Crea un nuovo elemento di errore usando jQuery
+            let error = $('<p></p>')
+                .text('Name is required')
+                .attr('id', 'name-error')
+                .css({
+                    'color': 'darkred',
+                    'font-size': '0.8rem',
+                    'margin-bottom': '5px'
+                });
+            
+            // Inserisci l'elemento di errore dopo l'input 'name'
+            error.insertAfter(name);
+        } else {
+            // Rimuovi l'elemento di errore se presente
+            $('#name-error').remove();
+        }
+        checkRegistrationForm(userArray);
+    });
+
+    let lastname = $('#lastname');
+    lastname.on('blur', function(){
+        if (lastname.val() === ''){
+            // Rimuovi eventuali messaggi di errore precedenti
+            $('#lastname-error').remove();
+            
+            // Crea un nuovo elemento di errore usando jQuery
+            let error = $('<p></p>')
+                .text('Lastname is required')
+                .attr('id', 'lastname-error')
+                .css({
+                    'color': 'darkred',
+                    'font-size': '0.8rem',
+                    'margin-bottom': '5px'
+                });
+            
+            // Inserisci l'elemento di errore dopo l'input 'lastname'
+            error.insertAfter(lastname);
+        } else {
+            // Rimuovi l'elemento di errore se presente
+            $('#lastname-error').remove();
+        }
+        checkRegistrationForm(userArray);
+    });
+
+    let email = $('#email');
+    email.on('blur', function(){
+        if (email.val() === ''){
+            // Rimuovi eventuali messaggi di errore precedenti
+            $('#email-error').remove();
+            
+            // Crea un nuovo elemento di errore usando jQuery
+            let error = $('<p></p>')
+                .text('Email is required')
+                .attr('id', 'email-error')
+                .css({
+                    'color': 'darkred',
+                    'font-size': '0.8rem',
+                    'margin-bottom': '5px'
+                });
+            
+            // Inserisci l'elemento di errore dopo l'input 'email'
+            error.insertAfter(email);
+        } else if (email.val().indexOf('@') === -1 || email.val().indexOf('.') === -1){
+            // Rimuovi eventuali messaggi di errore precedenti
+            $('#email-error').remove();
+            
+            // Crea un nuovo elemento di errore usando jQuery
+            let error = $('<p></p>')
+                .text('Email is not valid')
+                .attr('id', 'email-error')
+                .css({
+                    'color': 'darkred',
+                    'font-size': '0.8rem',
+                    'margin-bottom': '5px'
+                });
+            
+            // Inserisci l'elemento di errore dopo l'input 'email'
+            error.insertAfter(email);
+        } else {
+            // Rimuovi l'elemento di errore se presente
+            $('#email-error').remove();
+        }
+        checkRegistrationForm(userArray);
+    });
+
+    let username = $('#username');
+    username.on('blur', function(){
+        if (username.val() === ''){
+            // Rimuovi eventuali messaggi di errore precedenti
+            $('#username-error').remove();
+            
+            // Crea un nuovo elemento di errore usando jQuery
+            let error = $('<p></p>')
+                .text('Username is required')
+                .attr('id', 'username-error')
+                .css({
+                    'color': 'darkred',
+                    'font-size': '0.8rem',
+                    'margin-bottom': '5px'
+                });
+            
+            // Inserisci l'elemento di errore dopo l'input 'username'
+            error.insertAfter(username);
+        } else {
+            // Rimuovi l'elemento di errore se presente
+            $('#username-error').remove();
+        }
+        checkRegistrationForm(userArray);
+    });
+
+    let password = $('#password');
+    password.on('blur', function(){
+        if (password.val() === ''){
+            // Rimuovi eventuali messaggi di errore precedenti
+            $('#password-error').remove();
+            
+            // Crea un nuovo elemento di errore usando jQuery
+            let error = $('<p></p>')
+                .text('Password is required')
+                .attr('id', 'password-error')
+                .css({
+                    'color': 'darkred',
+                    'font-size': '0.8rem',
+                    'margin-bottom': '5px'
+                });
+            
+            // Inserisci l'elemento di errore dopo l'input 'password'
+            error.insertAfter(password);
+        } else {
+            // Rimuovi l'elemento di errore se presente
+            $('#password-error').remove();
+        }
+        checkRegistrationForm(userArray);
+    });
+
+    let confirmPassword = $('#confirm-password');
+    confirmPassword.on('blur', function(){
+        if (confirmPassword.val() === ''){
+            // Rimuovi eventuali messaggi di errore precedenti
+            $('#confirm-password-error').remove();
+            
+            // Crea un nuovo elemento di errore usando jQuery
+            let error = $('<p></p>')
+                .text('Confirm password is required')
+                .attr('id', 'confirm-password-error')
+                .css({
+                    'color': 'darkred',
+                    'font-size': '0.8rem',
+                    'margin-bottom': '5px'
+                });
+            
+            // Inserisci l'elemento di errore dopo l'input 'confirm-password'
+            error.insertAfter(confirmPassword);
+        } else if (confirmPassword.val() !== password.val()){
+            // Rimuovi eventuali messaggi di errore precedenti
+            $('#confirm-password-error').remove();
+            
+            // Crea un nuovo elemento di errore usando jQuery
+            let error = $('<p></p>')
+                .text('Passwords do not match')
+                .attr('id', 'confirm-password-error')
+                .css({
+                    'color': 'darkred',
+                    'font-size': '0.8rem',
+                    'margin-bottom': '5px'
+                });
+            
+            // Inserisci l'elemento di errore dopo l'input 'confirm-password'
+            error.insertAfter(confirmPassword);
+        } else {
+            // Rimuovi l'elemento di errore se presente
+            $('#confirm-password-error').remove();
+        }
+        checkRegistrationForm(userArray);
+    });
+
+    let age = $('#age');
+    age.on('blur', function(){
+        if (age.val() === ''){
+            // Rimuovi eventuali messaggi di errore precedenti
+            $('#age-error').remove();
+            age.attr('class', '');
+            
+            // Crea un nuovo elemento di errore usando jQuery
+            let error = $('<p></p>')
+                .text('Age is required')
+                .attr('id', 'age-error')
+                .css({
+                    'color': 'darkred',
+                    'font-size': '0.8rem',
+                    'margin-bottom': '5px'
+                });
+            
+            // Inserisci l'elemento di errore dopo l'input 'age'
+            error.insertAfter(age);
+        } else {
+            // Rimuovi l'elemento di errore se presente
+            $('#age-error').remove();
+            age.attr('class', 'is-full');
+        }
+        checkRegistrationForm(userArray);
+    });
+
+    let accountImage = $('#account-image');
+    accountImage.on('change', function(){
+        console.log(accountImage.val());
+        if (accountImage.val() === ''){
+            accountImage.attr('class', '');
+        } else {
+            // Rimuovi l'elemento di errore se presente
+            accountImage.attr('class', 'is-full');
         }
         checkRegistrationForm(userArray);
     });
--- login.php
+++ login.php
@@ -12,6 +12,7 @@
 
         $userArray = json_decode(file_get_contents('./resources/database/user.json'), true);
         $login_error = false;
+        $register_error = false;
 
         if($_GET['state'] === 'login' && isset($_POST['username']) && isset($_POST['password'])) {
             $username = $_POST['username'];
@@ -25,6 +26,85 @@
             }
         } else {
             $login_error = true;
+        }
+
+        if ($_GET['state'] === 'register' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Recupera i dati del form
+            $nome = $_POST['name'];
+            $cognome = $_POST['lastname'];
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $data_di_nascita = $_POST['age'];
+            $password = $_POST['password'];
+            $confirm_password = $_POST['confirm-password'];
+
+            // Verifica che le password corrispondano
+            if ($password !== $confirm_password) {
+                $register_error = true;
+                // Gestisci l'errore
+            }
+
+            // Gestisci il caricamento dell'immagine
+            if (isset($_FILES['account-image']) && $_FILES['account-image']['error'] === UPLOAD_ERR_OK) {
+                $fileTmpPath = $_FILES['account-image']['tmp_name'];
+                $fileName = $_FILES['account-image']['name'];
+                $fileSize = $_FILES['account-image']['size'];
+                $fileType = $_FILES['account-image']['type'];
+                $fileNameCmps = explode(".", $fileName);
+                $fileExtension = strtolower(end($fileNameCmps));
+
+                // Specifica le estensioni consentite
+                $allowedfileExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
+                if (in_array($fileExtension, $allowedfileExtensions)) {
+                    // Crea un nome unico per il file
+                    $newFileName = $username . '_' . uniqid() . '.' . $fileExtension;
+
+                    // Directory di destinazione
+                    $uploadFileDir = './resources/img/';
+                    $dest_path = $uploadFileDir . $newFileName;
+
+                    // Sposta il file nella directory di destinazione
+                    if(move_uploaded_file($fileTmpPath, $dest_path)) {
+                        $avatarPath = $dest_path;
+                    } else {
+                        $register_error = true;
+                        // Gestisci l'errore nel caricamento
+                    }
+                } else {
+                    $register_error = true;
+                    // Gestisci il tipo di file non valido
+                }
+            }
+
+            if (!$register_error) {
+                // Crea il nuovo utente
+                $newUser = [
+                    "id" => time(), // O utilizza un metodo per generare ID unici
+                    "nome" => $nome,
+                    "cognome" => $cognome,
+                    "data_di_nascita" => $data_di_nascita,
+                    "username" => $username,
+                    "password" => password_hash($password, PASSWORD_BCRYPT), // Hash sicuro della password
+                    "email" => $email,
+                    "avatar" => isset($avatarPath) ? $avatarPath : null
+                ];
+
+                // Leggi il file JSON esistente
+                $userJsonPath = './resources/database/user.json';
+                $jsonData = file_get_contents($userJsonPath);
+                $users = json_decode($jsonData, true);
+
+                // Aggiungi il nuovo utente
+                $users[] = $newUser;
+
+                // Scrivi di nuovo nel file JSON
+                file_put_contents($userJsonPath, json_encode($users, JSON_PRETTY_PRINT));
+
+                // Reindirizza al login
+                header('Location: login.php?selected=3&state=login'); 
+                exit();
+            }
         }
     ?>    
     <main>
@@ -55,24 +135,27 @@
             ?>
         <?php elseif($_GET['state'] === 'register'): ?>
             <section id="register-form">
-                <form action="login.php?selected=3&state=register" method="post">
+                <form action="login.php?selected=3&state=register" method="post" novalidate>
                     <h1>Register</h1>
                     <label for="Name">Name * :</label>
-                    <input type="text" name="name" id="name" required placeholder="insert your name">
+                    <input type="text" name="name" id="name" required placeholder="insert your name" autocomplete="off">
                     <label for="lastname">Lastname * :</label>
-                    <input type="text" name="lastname" id="lastname" required placeholder="insert your lastname">
+                    <input type="text" name="lastname" id="lastname" required placeholder="insert your lastname" autocomplete="off">
                     <label for="email">Email * : </label>
-                    <input type="email" name="email" id="email" required placeholder="insert your email">
+                    <input type="email" name="email" id="email" required placeholder="insert your email" autocomplete="off">
                     <label for="username">Username * : </label>
-                    <input type="text" name="username" id="username" required placeholder="insert your username">
+                    <input type="text" name="username" id="username" required placeholder="insert your username" autocomplete="off">
                     <label for="age">Birth date * : </label>
-                    <input type="date" name="age" id="age" required placeholder="insert your birth date">
+                    <input type="date" name="age" id="age" required placeholder="insert your birth date" autocomplete="off">
                     <label for="password">Password * : </label>
-                    <input type="password" name="password" id="password" required placeholder="insert your password">
+                    <input type="password" name="password" id="password" required placeholder="insert your password" autocomplete="off">
                     <label for="confirm-password">Confirm Password * : </label>
-                    <input type="password" name="confirm-password" id="confirm-password" required placeholder="insert your password">
+                    <input type="password" name="confirm-password" id="confirm-password" required placeholder="insert your password" autocomplete="off">
                     <label for="account-image">Insert your account image : </label>
-                    <input type="file" name="account-image" id="account-image">
+                    <input type="file" name="account-image" id="account-image" accept="image/*">
+                    <?php if($register_error): ?>
+                        <p class="error">Registration Gone Wrong</p>
+                    <?php endif; ?>
                     <button type="submit" disabled>Register</button>
                 </form>
             </section>
